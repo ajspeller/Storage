ec16b5af77dba398ae9d2d9d181d5d2716d03ff4
AJ Speller <ajspeller@gmail.com>
2017-08-02 13:55:23 -0400

    Play around with variables and MessageBox

---
+++ Storage/FrmStorage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Storage
+{
+    public partial class FrmStorage : Form
+    {
+        // variable declarations
+        int age;
+        string yourName;
+        double pay;
+
+        public FrmStorage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            // assignments
+            age = 43;
+            yourName = "AJ Speller";
+            pay = 134.77;
+
+            // display results
+            MessageBox.Show("Your age is " + age.ToString());
+            MessageBox.Show("Your name is " + yourName);
+            MessageBox.Show("Your pay is " + pay.ToString());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your name is " + yourName);
+
+        }
+    }
+}
--- Storage/Program.cs
+++ Storage/Program.cs
@@ -16,7 +16,7 @@ namespace Storage
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmStorage());
         }
     }
 }
